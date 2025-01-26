f6c8b13222aa2fa7fbe075e9ad75b7edcc3e978b
Lenny Thobejane <Lennythobejane@gmail.com>
2025-01-26 12:03:21 +0200

    category filter fix

--- src/app/components/FilterByCategory.js
+++ src/app/components/FilterByCategory.js
@@ -3,28 +3,33 @@
 // MUI
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 
 // Utils
 
 const FilterByCategory = ({ setFilters, filters }) => {
     return (
-        <Select
-            label="Category"
-            variant="outlined"
-            fullWidth
-            defaultValue=""
-            sx={{
-                width: "100%",
-            }}
-            onChange={(e) => setFilters({ ...filters, category: e.target.value })}
-        >
-            <MenuItem value="">
-                <em>All</em>
-            </MenuItem>
-            <MenuItem value={"personal"}>Personal</MenuItem>
-            <MenuItem value={"tech"}>Tech</MenuItem>
-            <MenuItem value={"travel"}>Travel</MenuItem>
-        </Select>
+        <FormControl fullWidth>
+            <InputLabel>Category</InputLabel>
+            <Select
+                label="Category"
+                variant="outlined"
+                fullWidth
+                defaultValue=""
+                sx={{
+                    width: "100%",
+                }}
+                onChange={(e) => setFilters({ ...filters, category: e.target.value })}
+            >
+                <MenuItem value="">
+                    <em>All</em>
+                </MenuItem>
+                <MenuItem value={"personal"}>Personal</MenuItem>
+                <MenuItem value={"tech"}>Tech</MenuItem>
+                <MenuItem value={"travel"}>Travel</MenuItem>
+            </Select>
+        </FormControl>
     );
 };
 
